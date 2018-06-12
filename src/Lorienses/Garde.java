ea0c79c18dba9c93307592c97b87e041a905b80a
caceres06 <37302560+caceres06@users.noreply.github.com>
2018-06-12 15:14:50 -0600

    se agrego interface y clases

---
+++ src/Lorienses/Garde.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lorienses;
+
+import segundoparcialpoo.Ejercito;
+
+/**
+ *
+ * @author Mabel
+ */
+public class Garde implements Ejercito{
+    @Override
+    public void Entrenar(){
+        System.out.println("El escuadron GARDE esta siendo entrenado");
+    }
+}
